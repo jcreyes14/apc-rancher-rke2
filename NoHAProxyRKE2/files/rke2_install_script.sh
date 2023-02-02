8b5c1028e8e36f8c654bfd7c7acb7c8818a167bb
jcreyes14 <reyesjcv@gmail.com>
2023-02-02 11:56:01 -0600

    Update and rename script.sh to rke2_install_script.sh

--- NoHAProxyRKE2/files/rke2_install_script.sh
+++ NoHAProxyRKE2/files/rke2_install_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo curl -sfL https://get.rke2.io | sh -
+sudo curl -sfL https://get.rke2.io | sudo sh -
 sudo systemctl enable rke2-server.service
 n=0
-until [ "$n" -ge 5 ]
+until [ "$n" -ge 3 ]
 do
    sudo systemctl start rke2-server.service && break  # substitute your command here
    n=$((n+1)) 
-   sleep 60
+   sleep 20
 done
